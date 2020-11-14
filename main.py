e6f3cd70ffa257cea271ae43e7a99ae22665a0b4
ochdev <contact@och.dev>
2020-11-14 22:00:50 +0100

    Fix poetry numpy and matplotlib issues when installing

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-# This is a sample Python script.
+import numpy as np
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    a = np.arange(15).reshape(3, 5)
+    print(a)
